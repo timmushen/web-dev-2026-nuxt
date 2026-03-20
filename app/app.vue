f91df4a9a46845fa16a0ff55e0da56936492459e
timmushen <t@x.enterprises>
2026-03-19 20:03:01 -0700

    Init

---
+++ app/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="flex flex-row justify-between gap-4 max-w-6xl mx-auto">
+    <Bucket title="Title Here" className="bg-green-500">
+        <strong>Lorem ipsum dolor sit amet consectetur</strong> 
+      
+        adipisicing elit. Voluptates voluptatum dolore at consequuntur assumenda voluptas quod quibusdam, nihil saepe harum, atque labore odio ex eum eius nam ipsam? Eligendi, animi?
+      </Bucket>
+    <Bucket title="Column 2" className="bg-gray-600" />
+     <Bucket>slot</Bucket>
+ 
+  </section>
+</template>
---
+++ app/components/Bucket.vue
@@ -0,0 +1,19 @@
+<template>
+    <article class=" flex-1 p-6 rounded-xl" :class="className">
+      <div class="font-bold text-xl">{{title}}</div>
+      <div><slot/></div>
+    </article>
+</template>
+<script setup>
+const props = defineProps({
+    title:{
+        default: 'No Value'
+    }, 
+    body:{
+        default: ''
+    }, 
+    className:{
+        default: 'bg-red-600'
+    }
+})
+</script>
